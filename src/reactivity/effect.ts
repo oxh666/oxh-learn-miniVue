87896a09e46429ceb8597f70c178461c40c92910
oxh666 <jojo5981@outlook.com>
2023-11-21 16:01:01 +0800

    07-实现 effect 返回 runner 08-实现 effect 的scheduler 功能 09-实现 effect 的 stop 功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,26 +1,56 @@
+import { extend } from "../shared";
 //全局变量
 /**
  * 1. 依赖收集
  */
 let activeEffect: any;
 
-
 /**
  * @description  依赖收集
  */
 const targetMap = new Map();
 
-
 //类
 class ReactiveEffect {
-  private _fn: any;
-  constructor(fn: any) {
+  active = true
+  deps = [];//存放dep
+  onStop?: () => void
+  private _fn: any; //用户传入的函数
+  public scheduler: Function | undefined; //调度程序
+  
+  
+  constructor(fn: any, public scheduler?: any) {
     this._fn = fn;
   }
+  
   run() {
     activeEffect = this;
     return this._fn();
   }
+  
+  stop() {
+    if (this.active) {
+      //将当前的effect从dep中移除
+      cleanupEffect(this);
+      //如果有onStop，就执行onStop
+      if (this.onStop) {
+        //
+        this.onStop()
+      }
+      this.active = false
+    }
+  }
+}
+
+/**
+ * @description 清除effect|effect.stop功能实现
+ * @param effect
+ */
+function cleanupEffect(effect: any) {
+  //将当前的effect从dep中移除
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  })
 }
 
 /**
@@ -37,7 +67,7 @@ export function track(target: any, key: any) {
     depsMap = new Map();
     targetMap.set(target, depsMap);
   }
-
+  
   //拿到 key -> dep
   let dep = depsMap.get(key);
   //key没有重复的，就创建一个set
@@ -45,33 +75,54 @@ export function track(target: any, key: any) {
     dep = new Set();
     depsMap.set(key, dep);
   }
+  if (!activeEffect) return;
   //将当前的effect 放到dep中
   dep.add(activeEffect);
+  //将dep放到effect中
+  activeEffect.deps.push(dep);
+  
 }
 
 
 /**
  * @description 触发依赖
- * @param target 
- * @param key 
+ * @param target
+ * @param key
  */
 export function trigger(target: any, key: any) {
   //根据 target和key去获取依赖OBJECT（dep),然后遍历收集之前的fn执行
   let depsMap = targetMap.get(target);
   let dep = depsMap.get(key);
-
+  
   for (const effect of dep) {
-    effect.run()
+    //如果有scheduler，就执行scheduler
+    if (effect.scheduler) {
+      effect.scheduler
+    } else {
+      effect.run()
+    }
   }
-
+  
 }
+
 /**
  *
- * @param fn
+ * @param fn 用户传入的函数
+ * @param options 选项
  */
-export function effect(fn: any) {
-  const _effect = new ReactiveEffect(fn);
+export function effect(fn: any, options: any = {}) {
+  const _effect: any = new ReactiveEffect(fn, options.scheduler);
+  // _effect.onStop = options.onStop
+  //优雅赋值
+  extend(_effect, options);
   _effect.run();
   //处理指针
-  return _effect.run.bind(_effect);
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
+  
+  return runner;
+}
+
+export function stop(runner: any) {
+  runner.effect.stop();
 }
--- src/tests/effect.spec.ts
+++ src/tests/effect.spec.ts
@@ -1,39 +1,118 @@
-import { it, expect, describe } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { reactive } from '../reactivity/reactive'
-import { effect } from '../reactivity/effect'
+import { effect,stop } from '../reactivity/effect'
+
 describe('effect', () => {
-    it('happy_path', () => {
-       //given
-        const user=reactive({name:'John',age:10})
-        let nextAge
-
-        
-         effect (()=>{
-            nextAge=user.age+1
-        }
-        )
-        expect(nextAge).toBe(11)
-
-        //update  没有收集依赖和触发依赖以下测试代码报错
-        user.age++
-        expect(nextAge).toBe(12)
-
-    })
-
-    it('should return runner when call effect',()=>{
-        //1 effect(fn)-> function runner()-> fn -> return
-        //1.given
-        let foo=10
-      const runner=  effect(()=>{
-            foo++
-            return 'foo'
-        })
-        expect(foo).toBe(11)
-        //runner() 是否会执行fn
-        const r=runner()
-        expect(foo).toBe(12)
-        //runner() 是否会返回fn的返回值 
-        expect(r).toBe('foo') 
-    })
-})
+  it('happy_path', () => {
+    //given
+    const user = reactive({name: 'John', age: 10})
+    let nextAge
+    
+    
+    effect(() => {
+        nextAge = user.age + 1
+      }
+    )
+    expect(nextAge).toBe(11)
+    expect(2).toBe(2)
+    
+    //update  没有收集依赖和触发依赖以下测试代码报错
+    user.age++
+    expect(nextAge).toBe(12)
+    
+  })
   
+  /**
+   *@description effect内部的 runner作用
+   * 1. effect(fn) -> function runner() -> fn -> return
+   * 2. 调用runner可以拿到fn的返回值
+   * 3. runner() 会执行fn
+   * 4. runner() 会返回fn的返回值
+   */
+  it('should return runner when call effect', () => {
+    //1.given
+    let foo = 10
+    const runner = effect(() => {
+      foo++
+      return 'foo'
+    })
+    expect(foo).toBe(11)
+    //runner() 是否会执行fn
+    const r = runner()
+    expect(foo).toBe(12)
+    //runner() 是否会返回fn的返回值
+    expect(r).toBe('foo')
+  })
+  
+  /**
+   * @description 测试简介scheduler（ 调度程序）
+   * 1. 通过 effect 的第二个参数给定的一个 scheduler 的 fn
+   * 2. effect 第一次执行的时候还会执行 fn（参数1）
+   * 3. 当响应式对象 set  update 的时候，不会执行 fn，而是执行 scheduler
+   * 4. 如果执行 scheduler，那么会再次执行fn
+   */
+  it('scheduler', () => {
+    //1.given
+    let dummy // 用于收集依赖
+    let run: any // 用于执行scheduler
+    
+    const scheduler = vi.fn(() => {
+      run = runner
+    })//
+    const obj = reactive({foo: 1})
+    //2.when
+    const runner = effect(
+      () => {
+        dummy = obj.foo
+      },
+      {scheduler}
+    )
+    //3.then
+    expect(scheduler).not.toHaveBeenCalled()//第一次执行时不会被调用
+    expect(dummy).toBe(1)
+    // should be called on first trigger 第一次相应后会被调用
+    obj.foo++
+    expect(scheduler).toHaveBeenCalledTimes(1)
+    // should not run yet
+    expect(dummy).toBe(1)
+    console.log('run', run)
+    // manually run
+    run()
+    // should have run
+    expect(dummy).toBe(2)
+  })
+  
+  /**
+   * @description 测试stop
+   *
+    */
+  it('stop', () => {
+    //given
+    let dummy // 用于收集依赖
+    const obj = reactive({prop: 1}) //
+    //when
+    const runner = effect(() => {
+      dummy = obj.prop
+    })
+    obj.prop = 2
+    expect(dummy).toBe(2)
+    stop(runner)
+    obj.prop = 3
+    expect(dummy).toBe(2)
+    
+    // stopped effect should still be manually callable
+    runner()
+    expect(dummy).toBe(3)
+  })
+  
+  it('events: onStop', () => {
+    const onStop = vi.fn()
+    const runner = effect(() => {}, {
+      onStop
+    })
+    
+    stop(runner)
+    expect(onStop).toHaveBeenCalled()
+  })
+  
+})
